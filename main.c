ce6adc9032351d6ae4189f407bf5ae9af63e7f23
Nihar Godbole <nihargodbole@microsoft.com>
2022-06-21 23:06:11 +0530

    Implementing Part B

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+// Macros for API spec.
 
 #define LEFT_OP_MASK        (0xf)
 #define ADD_MASK            (1 << 4)
@@ -25,8 +26,88 @@
 #define BYTE_MAX            (127)
 #define BYTE_MIN            (-128)
 
-extern bool readKnobInputs(unsigned *dataPtr);
-extern void writeLedOutput(unsigned data);
+
+// HW spec defines.
+
+#define USER_REG_INP_1          (0xa0000000)
+#define USER_REG_INP_2          (0xa0000004)
+#define USER_REG_OUT_1          (0xa0000008)
+
+volatile const int *userInpReg1 = USER_REG_INP_1;
+volatile const int *userInpReg2 = USER_REG_INP_2;
+
+int *userOutReg1 = USER_REG_OUT_1;
+
+#define HW_POS_ADD      (0)
+#define HW_POS_SUB      (1)
+#define HW_POS_MUL      (2)
+#define HW_POS_DIV      (3) 
+
+bool readKnobInputs(unsigned *dataPtr)
+{
+    int inp_reg1_val = *userInpReg1;
+    int inp_reg2_val = *userInpReg2;
+    int left_operand = 0;
+    int right_operand = 0;
+
+    if (dataPtr == NULL)
+        return false;
+
+    // get left operand.
+    int value = inp_reg1_val >> 16;
+    for (int i = 0; i < 16; i++)
+    {
+        if (value & 1 == 1) 
+        {
+            left_operand = i;
+            break;
+        }
+        value = value >> 1;
+    }
+    
+
+    // get right operand. 
+    value = value & 0xffff;
+    for (int i = 0; i < 16; i++)
+    {
+        if (value & 1 == 1)
+        {
+            right_operand = i;
+            break;
+        }
+        value = value >> 1;
+    }
+
+    *dataPtr = 0;
+    *dataPtr = *dataPtr | ((*dataPtr & LEFT_OP_MASK) | (left_operand << LEFT_OP_SHIFT));
+    *dataPtr = *dataPtr | ((*dataPtr & RIGHT_OP_MASK) | (right_operand << RIGHT_OP_SHIFT));
+    
+    // get operation type
+    if (inp_reg2_val & (1 << HW_POS_ADD))
+    {
+        *dataPtr = *dataPtr | ((*dataPtr & ADD_MASK) | (1 << ADD_SHIFT));
+    }
+    if (inp_reg2_val & (1 << HW_POS_SUB))
+    {
+        *dataPtr = *dataPtr | ((*dataPtr & SUB_MASK) | (1 << SUB_SHIFT));
+    }
+    if (inp_reg2_val & (1 << HW_POS_MUL))
+    {
+        *dataPtr = *dataPtr | ((*dataPtr & MUL_MASK) | (1 << MUL_SHIFT));
+    }
+    if (inp_reg2_val & (1 << HW_POS_DIV))
+    {
+        *dataPtr = *dataPtr | ((*dataPtr & DIV_MASK) | (1 << DIV_SHIFT));
+    }
+
+    return true;
+    
+}
+
+void writeLedOutput(unsigned data)
+{
+    *userOutReg1 = data;
+}
 
 int main(void)
 {
